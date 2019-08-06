2813f3eca30010848b88b057842ddd0e5fe15098
Imaraika <inange2013@gmail.com>
2019-08-06 10:22:03 +0200

    changes in js file

--- js/script.js
+++ js/script.js
@@ -10,30 +10,38 @@ $(document).ready(function() {
     };
     //USER INTERFACE
     $("#sb").click(function() {
-        $("#myForm").submit(); // Submit the form
-    
-        var size = $('#sizeSelector').val();
-        var crust = $('#crustSelector').val();
-        console.log(size);
-        var toppings = [];
-        $("input[type=checkbox]:checked").each(function() {
-            toppings.push($(this).val());
+        $("#myForm").submit(function(event){
+            event.preventDefault();
+            var size = $('#sizeSelector').val();
+            var crust = $('#crustSelector').val();
+            console.log(size);
+            var toppings = [];
+            $("input[type=checkbox]:checked").each(function() {
+                toppings.push($(this).val());
+            });
+            console.log(toppings);
+            var newOrder = new placeOrder(size, crust, toppings);
+            console.log(newOrder);
+            $('ul#placeorderlist').append("<li>" + newOrder.summary() + "</li>");
+            var anotherTopping = 0;
+            for (var i = 0; i < toppings.length; i++) {
+                anotherTopping += parseInt(toppings[i]);
+            }
+            var finalTotal = parseInt(size) + parseInt(crust) + parseInt(anotherTopping);
+           alert(finalTotal);
+            $("#total").append("<p>"+finalTotal+"</p>");
         });
-        console.log(toppings);
-        var newOrder = new placeOrder(size, crust, toppings);
-        console.log(newOrder);
-        $('ul#placeorderlist').append("<li>" + newOrder.summary() + "</li>");
-        var anotherTopping = 0;
-        for (var i = 0; i < toppings.length; i++) {
-            anotherTopping += parseInt(toppings[i]);
-        }
-        var finalTotal = parseInt(size) + parseInt(crust) + parseInt(anotherTopping);
-       alert(finalTotal);
-        $("#total").append("<p>"+finalTotal+"</p>");
-    });
-    $('#img').click(function() {
-        alert("the delivery cost is 2000");
-        var a = prompt("enter ur location");
-        alert("your order will be delivered to" + a);
-    });
+        $('#img').click(function() {
+            alert("the delivery cost is 2000");
+            var a = prompt("enter ur location");
+            alert("your order will be delivered to" + a);
+        });
+        }) // Submit the form
+  
  });
+
+// $(document).ready(function() {
+//     $('#sb').click(function(){
+//         alert("you clicked me!");
+//     })
+// });
